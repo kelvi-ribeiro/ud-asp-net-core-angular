61ce8026165f34f8cd1efa653613beea6088f7f3
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2019-12-26 20:30:01 -0300

    Aula 03. Iniciando o Jogo

--- console-core/GameTOP/Program.cs
+++ console-core/GameTOP/Program.cs
@@ -2,11 +2,25 @@
 
 namespace GameTOP
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      var jogo = new JogoFODA("Cristiano Ronaldo");
+      jogo.IniciarJogo();
     }
+  }
+  class JogoFODA
+  {
+    private readonly string _NomeJogador;
+
+    public JogoFODA(string nome)
+    {
+      _NomeJogador = nome;
+    }
+    public void IniciarJogo()
+    {
+      Console.Write($"{_NomeJogador} deu um passe");
+    }
+  }
 }
