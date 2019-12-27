8713b27be4dedfae0ae2015dd1b9da80b9c5e1ec
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2019-12-27 02:38:16 -0300

    Aula 05. O Acoplamento

---
+++ console-core/GameTOP/Jogador1.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace GameTOP
+{
+    public class Jogador1
+  {
+    public Jogador1(string nome)
+    {
+      _Nome = nome;
+    }
+    public readonly string _Nome;
+
+    public string Chuta()
+    {
+      return $"{_Nome} está chutando \n";
+    }
+
+    public string Corre()
+    {
+      return $"{_Nome} está correndo \n";
+    }
+
+    public string Passa()
+    {
+      return $"{_Nome} está passando \n";
+    }
+  }
+}
--- console-core/GameTOP/Program.cs
+++ console-core/GameTOP/Program.cs
@@ -9,28 +9,5 @@ namespace GameTOP
       var jogo = new JogoFODA(new Jogador("Ronaldo"));
       jogo.IniciarJogo();
     }
-  }
-  public class Jogador
-  {
-    public Jogador(string nome)
-    {
-      _Nome = nome;
-    }
-    public readonly string _Nome;
-
-    public void Chuta()
-    {
-      Console.Write($"{_Nome} está chutando");
-    }
-
-    public void Corre()
-    {
-      Console.Write($"{_Nome} está correndo");
-    }
-
-    public void Passa()
-    {
-      Console.Write($"{_Nome} está passando");
-    }
-  }
+  }  
 }
