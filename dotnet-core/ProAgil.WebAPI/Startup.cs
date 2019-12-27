c21e3278352fd56f27163d4c66005ac7a2e1e2cd
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2019-12-27 19:41:40 -0300

    Aula 14. Inicio do Projeto

--- dotnet-core/ProAgil.WebAPI/Startup.cs
+++ dotnet-core/ProAgil.WebAPI/Startup.cs
@@ -36,7 +36,7 @@ namespace ProAgil.WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
