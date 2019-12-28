3fd2576e7737b50e388bcb498bf97fac082bae8d
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2019-12-28 00:32:27 -0300

    Aula 19. Retornando Dados

--- dotnet-core/ProAgil.WebAPI/Controllers/WeatherForecastController.cs
+++ dotnet-core/ProAgil.WebAPI/Controllers/WeatherForecastController.cs
@@ -4,69 +4,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProAgil.WebAPI.Data;
 using ProAgil.WebAPI.Model;
 
 namespace ProAgil.WebAPI.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+  [ApiController]
+  [Route("[controller]")]
+  public class WeatherForecastController : ControllerBase
+  {
+    public readonly DataContext _context;
+    public WeatherForecastController(DataContext context)
     {
-        private static readonly string[] Summaries = new[]
-        {
+      _context = context;
+    }
+    private static readonly string[] Summaries = new[]
+    {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet]
-        public IEnumerable<Evento> Get()
-        {
-           return new Evento[] {
-               new Evento() {
-                   EventoId = 1,
-                   Tema = "Angular e .NET Core",
-                   Local = "Belo Horizonte",
-                   Lote = "Primeiro Lote",
-                   QtdPessoas = 250,
-                   DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-               },
-               new Evento() {
-                   EventoId = 2,
-                   Tema = "React e Java",
-                   Local = "Rio de Janeiro",
-                   Lote = "Segundo Lote Lote",
-                   QtdPessoas = 200,
-                   DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
-               }
-           };
-        }
-        [HttpGet("{id}")]
-        public ActionResult<Evento> Get(int id)
-        {
-           return new Evento[] {
-               new Evento() {
-                   EventoId = 1,
-                   Tema = "Angular e .NET Core",
-                   Local = "Belo Horizonte",
-                   Lote = "Primeiro Lote",
-                   QtdPessoas = 250,
-                   DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-               },
-               new Evento() {
-                   EventoId = 2,
-                   Tema = "React e Java",
-                   Local = "Rio de Janeiro",
-                   Lote = "Segundo Lote Lote",
-                   QtdPessoas = 200,
-                   DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
-               }
-           }.FirstOrDefault(x => x.EventoId == id);
-        }
+    [HttpGet]
+    public IEnumerable<Evento> Get()
+    {
+      /* return _context.Eventos.ToList();Funciona de qualquer jeito */
+      return _context.Eventos;
     }
+    [HttpGet("{id}")]
+    public ActionResult<Evento> Get(int id)
+    {
+      return _context.Eventos.FirstOrDefault(x => x.EventoId == id);
+    }
+  }
 }
