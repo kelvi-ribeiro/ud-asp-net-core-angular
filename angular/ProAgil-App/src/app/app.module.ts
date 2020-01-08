cf64b730907cb8a85ab4b5242b05dc45a45a3c3b
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2020-01-08 03:45:47 -0300

    Aula 61. Botões e Tooltips

--- angular/ProAgil-App/src/app/app.module.ts
+++ angular/ProAgil-App/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
+import { HttpClientModule } from '@angular/common/http'
 import { BsDropdownModule, TooltipModule, ModalModule } from "ngx-bootstrap";
 
-import { HttpClientModule } from '@angular/common/http'
+import { EventosComponent } from './eventos/eventos.component';
+import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+
+
 import { DateTimeFormatTimePipe } from './helps/DateTimeFormatTime.pipe';
 
 @NgModule({
