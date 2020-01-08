5bcac06782a277e70eba86a52bdd528b780d6e8f
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2020-01-08 04:22:27 -0300

    Aula 66. Validação do Forms - Parte 01

--- angular/ProAgil-App/src/app/eventos/eventos.component.ts
+++ angular/ProAgil-App/src/app/eventos/eventos.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { EventoService } from '../services/evento.service';
 import { Evento } from '../models/Evento';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap';
-import { FormGroup, FormControl, } from '@angular/forms';
+import { FormGroup, FormControl, Validators, } from '@angular/forms';
 
 @Component({
   selector: 'app-eventos',
@@ -16,12 +16,12 @@ export class EventosComponent implements OnInit {
   imagemMargem = 2
   mostrarImagem = false
   modalRef: BsModalRef
-  registerForm:FormGroup
+  registerForm: FormGroup
 
   constructor(
-      private eventoService: EventoService,
-      private modalService:BsModalService
-      ) { }
+    private eventoService: EventoService,
+    private modalService: BsModalService
+  ) { }
 
   _filtroLista: string
   get filtroLista() {
@@ -39,6 +39,7 @@ export class EventosComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.validation()
     this.getEventos()
   }
 
@@ -46,18 +47,35 @@ export class EventosComponent implements OnInit {
     this.mostrarImagem = !this.mostrarImagem
   }
 
-  salvarAlteracao(){
-    
+  salvarAlteracao() {
+
   }
 
-  validation(){
+  validation() {
     this.registerForm = new FormGroup({
-      tema: new FormControl,
-      local: new FormControl,
-      dataEvento: new FormControl,
-      qtdPessoas: new FormControl,
-      telefone: new FormControl,
-      email: new FormControl,      
+      tema: new FormControl('',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(50)
+        ]
+      ),
+      local: new FormControl('', Validators.required),
+      dataEvento: new FormControl('', Validators.required),
+      imagemURL: new FormControl('', Validators.required),
+      qtdPessoas: new FormControl('',
+        [
+          Validators.required,
+          Validators.max(120000),
+        ]
+      ),
+      telefone: new FormControl('', Validators.required),
+      email: new FormControl('',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ),
     })
   }
 
