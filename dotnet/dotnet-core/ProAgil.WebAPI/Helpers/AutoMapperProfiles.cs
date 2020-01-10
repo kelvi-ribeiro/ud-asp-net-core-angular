cf20aaa1b0be77fdcbe86eb93ca1f62da193f7be
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2020-01-10 10:55:03 -0300

    Aula 81. Config Muitos para Muitos

--- dotnet/dotnet-core/ProAgil.WebAPI/Helpers/AutoMapperProfiles.cs
+++ dotnet/dotnet-core/ProAgil.WebAPI/Helpers/AutoMapperProfiles.cs
@@ -1,17 +1,26 @@
+using System.Linq;
 using AutoMapper;
 using ProAgil.Domain;
 using ProAgil.WebAPI.DTOs;
 
 namespace ProAgil.WebAPI.Helpers
 {
-    public class AutoMapperProfiles : Profile
+  public class AutoMapperProfiles : Profile
+  {
+    public AutoMapperProfiles()
     {
-        public AutoMapperProfiles()
+      CreateMap<Evento, EventoDto>()
+          .ForMember(dest => dest.Palestrantes, opt =>
+          {
+            opt.MapFrom(src => src.PalestrantesEventos.Select(x => x.Palestrante).ToList());
+          });
+      CreateMap<Palestrante, PalestranteDto>()
+        .ForMember(dest => dest.Eventos, opt =>
         {
-            CreateMap<Evento, EventoDto>();
-            CreateMap<Palestrante, PalestranteDto>();
-            CreateMap<Lote, LoteDto>();
-            CreateMap<RedeSocial, RedeSocialDto>();
-        }
+            opt.MapFrom(src => src.PalestrantesEventos.Select(x => x.Evento).ToList());
+        });
+      CreateMap<Lote, LoteDto>();
+      CreateMap<RedeSocial, RedeSocialDto>();
     }
+  }
 }
