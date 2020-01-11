2e03f0be6b73d442387aac6786dc457d2371ccfa
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2020-01-11 15:33:58 -0300

    Aula 94. Componente Título

--- angular/ProAgil-App/src/app/app.module.ts
+++ angular/ProAgil-App/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { NavComponent } from './nav/nav.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ContatosComponent } from './contatos/contatos.component';
+import { TituloComponent } from './_shared/titulo/titulo.component';
 
 
 import { DateTimeFormatTimePipe } from './helps/DateTimeFormatTime.pipe';
@@ -25,7 +26,8 @@ import { DateTimeFormatTimePipe } from './helps/DateTimeFormatTime.pipe';
       DateTimeFormatTimePipe,
       PalestrantesComponent,
       DashboardComponent,
-      ContatosComponent
+      ContatosComponent,
+      TituloComponent
    ],
    imports: [
       BrowserModule,
