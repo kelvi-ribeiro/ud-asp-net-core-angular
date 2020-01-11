0156dba300f00a7e3b7960a28ecf9cf5b1fc2783
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2020-01-11 20:29:44 -0300

    Aula 113. Configurando JWT

--- dotnet/dotnet-core/ProAgil.WebAPI/Startup.cs
+++ dotnet/dotnet-core/ProAgil.WebAPI/Startup.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,8 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using ProAgil.Domain.Identity;
 using ProAgil.Repository;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace ProAgil.WebAPI
 {
@@ -27,6 +30,7 @@ namespace ProAgil.WebAPI
     }
 
     public IConfiguration Configuration { get; }
+
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
@@ -51,6 +55,20 @@ namespace ProAgil.WebAPI
       builder.AddRoleManager<RoleManager<Role>>();
       builder.AddSignInManager<SignInManager<User>>();
 
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                    {
+                      options.TokenValidationParameters = new TokenValidationParameters
+                      {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                              .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                      };
+                    }
+                );
+
       services.AddScoped<IProAgilRepository, ProAgilRepository>();
       services.AddControllers();
       services.AddCors();
@@ -64,7 +82,7 @@ namespace ProAgil.WebAPI
             .Build();
             options.Filters.Add(new AuthorizeFilter(policy));
           })
-            .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)            
+            .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
             .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
     }
 
