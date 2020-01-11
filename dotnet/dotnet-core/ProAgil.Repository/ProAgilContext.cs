5ddca62a511405f442655c0e10a2dcd3666fc588
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2020-01-11 19:09:54 -0300

    Aula 110. Criando novo Banco

--- dotnet/dotnet-core/ProAgil.Repository/ProAgilContext.cs
+++ dotnet/dotnet-core/ProAgil.Repository/ProAgilContext.cs
@@ -6,12 +6,9 @@ using ProAgil.Domain.Identity;
 
 namespace ProAgil
 {
-  public class ProAgilContext : IdentityDbContext<User, Role, int,
-                                                        IdentityUserClaim<int>,
-                                                        UserRole,
-                                                        IdentityUserLogin<int>,
-                                                        IdentityRoleClaim<int>,
-                                                        IdentityUserToken<int>>
+    public class ProAgilContext : IdentityDbContext<User, Role, int,
+                                                    IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>,
+                                                    IdentityRoleClaim<int>, IdentityUserToken<int>>
 
   {
     public ProAgilContext(DbContextOptions<ProAgilContext> options) : base(options) { }
@@ -25,23 +22,25 @@ namespace ProAgil
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
       base.OnModelCreating(modelBuilder);
+
       modelBuilder.Entity<UserRole>(userRole =>
-      {
-        userRole.HasKey(usr => new { usr.UserId, usr.RoleId });
-        userRole.HasOne(usr => usr.Role)
-          .WithMany(r => r.UserRoles)
-          .HasForeignKey(ur => ur.RoleId)
-          .IsRequired();
+          {
+            userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
 
+            userRole.HasOne(ur => ur.Role)
+                      .WithMany(r => r.UserRoles)
+                      .HasForeignKey(ur => ur.RoleId)
+                      .IsRequired();
 
-        userRole.HasKey(usr => new { usr.UserId, usr.RoleId });
-        userRole.HasOne(usr => usr.Role)
-          .WithMany(r => r.UserRoles)
-          .HasForeignKey(ur => ur.UserId)
-          .IsRequired();
-      });
+            userRole.HasOne(ur => ur.User)
+                      .WithMany(r => r.UserRoles)
+                      .HasForeignKey(ur => ur.UserId)
+                      .IsRequired();
+          }
+      );
+
       modelBuilder.Entity<PalestranteEvento>()
-      .HasKey(PE => new { PE.EventoId, PE.PalestranteId });
+          .HasKey(PE => new { PE.EventoId, PE.PalestranteId });
     }
   }
 }
