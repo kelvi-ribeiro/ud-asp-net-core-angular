05549c2e2256c91dd9bfec28fc273a4753f89c85
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2020-01-12 03:31:26 -0300

    Aula 124. Tela Registrar Usuário

--- angular/ProAgil-App/src/app/user/registration/registration.component.ts
+++ angular/ProAgil-App/src/app/user/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup
+
+  constructor(
+    public fb: FormBuilder,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit() {
+    this.validation()
   }
 
+  validation() {
+    this.registerForm = this.fb.group({
+      fullName: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      userName: ['', Validators.required],
+      passwords: this.fb.group({
+        password: ['', Validators.required, Validators.minLength(4)],
+        confirmPassword: ['', Validators.required]
+      })
+      ,
+    })
+  }
+
+  cadastrarUsuario() {
+    console.log('cadastrarUsuario');
+
+  }
 }
