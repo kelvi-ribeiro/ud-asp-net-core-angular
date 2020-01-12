91b8d31846f054dddbee14526432e1b231a906ce
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2020-01-12 05:11:01 -0300

    Aula 131. Header Autenticado

--- angular/ProAgil-App/src/app/services/evento.service.ts
+++ angular/ProAgil-App/src/app/services/evento.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Evento } from '../models/Evento';
 
@@ -7,33 +7,36 @@ import { Evento } from '../models/Evento';
   providedIn: 'root'
 })
 export class EventoService {
-
+  tokenHeader : HttpHeaders
   baseURL = 'http://localhost:5000/api/eventos'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+    this.tokenHeader = new HttpHeaders({ 'Authorization': `Bearer ${localStorage.getItem('token')}` })
+  }
 
   getAllEventos(): Observable<Evento[]> {
-    return this.http.get<Evento[]>(this.baseURL)
+    
+    return this.http.get<Evento[]>(this.baseURL, { headers: this.tokenHeader })
   }
   getEventosByTema(tema: string): Observable<Evento[]> {
-    return this.http.get<Evento[]>(`${this.baseURL}/getByTema/${tema}`)
+    return this.http.get<Evento[]>(`${this.baseURL}/getByTema/${tema}`, { headers: this.tokenHeader })
   }
   getEventosById(id: number): Observable<Evento> {
-    return this.http.get<Evento>(`${this.baseURL}/${id}`)
+    return this.http.get<Evento>(`${this.baseURL}/${id}`, { headers: this.tokenHeader })
   }
   postUpload(file: File) {
     const fileToUpload = <File>file[0]
     const formData = new FormData()
     formData.append('file', fileToUpload, fileToUpload.name)
-    return this.http.post(`${this.baseURL}/upload`, formData)
+    return this.http.post(`${this.baseURL}/upload`, formData, { headers: this.tokenHeader })
   }
   postEvento(evento: Evento) {
-    return this.http.post(`${this.baseURL}`, evento)
+    return this.http.post(`${this.baseURL}`, evento, { headers: this.tokenHeader })
   }
   putEvento(evento: Evento) {
-    return this.http.put(`${this.baseURL}/${evento.id}`, evento)
+    return this.http.put(`${this.baseURL}/${evento.id}`, evento, { headers: this.tokenHeader })
   }
   deleteEvento(id: number) {
-    return this.http.delete(`${this.baseURL}/${id}`)
+    return this.http.delete(`${this.baseURL}/${id}`, { headers: this.tokenHeader })
   }
 }
